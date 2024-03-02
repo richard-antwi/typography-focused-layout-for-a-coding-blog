e457b3d2e865ba4d82fcb5143f44487b28987b93
Richard Antwi <richardrichfavourantwi88@gmail.com>
2024-03-02 17:49:48 +0000

    second commit

---
+++ src/components/Promo.js
@@ -0,0 +1,12 @@
+function Promo(){
+    return(
+        <div className= "promo-section">
+        <div>
+            <h1>Don't miss this deal!</h1>
+        </div>
+        <div>
+        </div>
+
+        </div>
+    );
+}
