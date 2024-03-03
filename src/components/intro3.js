e77538a28cbf584286a17c627ec169c3e8f8e3cb
Richard Antwi <richardrichfavourantwi88@gmail.com>
2024-03-03 21:45:33 +0000

    committing update

---
+++ src/components/intro3.js
@@ -0,0 +1,12 @@
+function Intro3(){
+    return(
+        <div className="blog-post-intro">
+            <h2> What's the best way to style your React App?</h2>
+            <div>
+                <p>There are so many options to choose from. Here's a high-level overview of the popular ones</p>
+                <p className="link">Read More ...</p>
+            </div>
+        </div>
+    );
+};
+export default Intro3;
