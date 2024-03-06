d843b111969b76aeda72fb598208e8b2af152417
Richard Antwi <richardrichfavourantwi88@gmail.com>
2024-03-06 19:24:49 +0000

    committing update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 // import { Component } from 'react';
-function Heading(props){
-
-  var title ="this is some heading text";
-  return(
-    <h1>{props.title}</h1>
-  );
-}
+import 'Promo' from '../Promo'
 function App() {
   return (
     <div className="App">
